b827cd526e4681e90354506cf30b04504ddbef91
Exoro123 <matte.nordin@live.se>
2023-03-09 13:15:24 +0100

    Added functionallity for add and sub

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ public class Main {
         System.out.println("Hello World");
     }
 
-    //public int add(int a, int b) { return a+b; }
+    public int add(int a, int b) { return a+b; }
 
-    //public int sub(int a, int b) { return a-b; }
+    public int sub(int a, int b) { return a-b; }
 
     //public int mul(int a, int b) { return a*b; }
 }
