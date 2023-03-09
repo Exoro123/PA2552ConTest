f349b303685934fbec1b7d25c1a244ad642f50fa
Exoro123 <matte.nordin@live.se>
2023-03-09 14:06:09 +0100

    Expanded tests for addition and subtraction Added tests for multiplication and division

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -31,11 +31,37 @@ public class TestMain {
     @Test
     public void testAdd() {
         assertEquals( 4, main.add(2,2));
+        assertEquals( 10, main.add(6,4));
+        assertEquals( 10, main.add(4,6));
+        assertEquals( 100, main.add(41,59));
+        assertEquals( -10, main.add(-4,-6));
+        assertEquals( -4, main.add(2,-6));
     }
 
     @Test
     public void testSub() {
         assertEquals( 0, main.sub(2,2));
+        assertEquals( -6, main.sub(2,8));
+        assertEquals( 0, main.sub(10,4));
+        assertEquals( -4, main.sub(-2,2));
+        assertEquals( 0, main.sub(-2,-2));
     }
 
+    @Test
+    public void testMul() {
+        assertEquals( 4, main.mul(2,2));
+        assertEquals( 15, main.mul(5,3));
+        assertEquals( -15, main.mul(5, -3));
+        assertEquals( 12, main.mul(-3,-4));
+        assertEquals( 0, main.mul(5,0));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals( 1, main.div(2,2));
+        assertEquals( 2, main.div(10,5));
+        assertEquals( -2, main.div(-10,5));
+        assertEquals( -2, main.div(10,-5));
+        assertEquals( 10, main.div(-100,-10));
+    }
 }
